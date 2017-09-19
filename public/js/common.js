fec137bfa91fad82c0af3bd82b0c798b1964922a
Sunmy000 <1043646173@qq.com>
2017-09-19 16:02:36 +0800

    完善登录退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,28 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+  // 实现退出功能
+$('#logoutBtn').click(function(){
+  $.ajax({
+    type : 'post',
+    url : '/api/logout',
+    dataType : 'json',
+    success : function(data){
+      console.log('123')
+      if(data.code == 200){
+        location.href = '/main/login';
+      }
+    }
+  });
+});
+
+// 验证用户是否登陆了
+var flag = $.cookie('PHPSESSID');
+if(!flag){
+  location.href = '/main/login';
+}
+// 填充头像信息
+var loginInfo = $.cookie('loginInfo');
+loginInfo = loginInfo && JSON.parse(loginInfo);
+$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+$('.aside .profile h4').html(loginInfo.tc_name);
