1bc2b306d1db5448d239396a4036edcea3763da1
Sunmy000 <1043646173@qq.com>
2017-09-21 17:42:42 +0800

    完成讲师启用和注销共功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -8,6 +8,27 @@ define(['jquery','template'],function ($,template) {
         //解析数据，渲染页面
         var html = template('teacherTpl',{list:data.result});
         $('#teacherInfo').html(html);
+        //启用和注销功能
+        $('.eod').click(function () {
+          var td = $(this).closest('td');
+          var tcId = td.attr('data-tcId');
+          var tcStatus = td.attr('data-status');
+          var that = this;
+          //调用后台接口
+          $.ajax({
+            type : 'get',
+            url : '/api/teacher/handle',
+            data : {tc_id : tcId, tc_status : tcStatus},
+            success : function (data) {
+              if (data.code == 200) {
+                td.attr('data-status', data.result.tc_status);
+                //因为是设置纯文本内容所以使用text()方法
+                $(that).text(data.result.tc_status ==0?'启用':'注销');
+
+              }
+            }
+          });
+        });
       }
     });
 });
