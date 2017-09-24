b2292ef5e1c2eea4e30022be7e064de1c43b06cd
Sunmy000 <1043646173@qq.com>
2017-09-24 11:14:28 +0800

    完成头像上传操作

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function ($, template) {
+define(['jquery','template','uploadify'],function ($, template) {
   //调用接口获取所有的个人信息
   $.ajax({
     type : 'get',
@@ -8,6 +8,23 @@ define(['jquery','template'],function ($, template) {
       //解析数据，渲染页面
       var html = template('settingsTpl',data.result);
       $('#settingsInfo').html(html);
+      //处理头像上传
+      $('#upfile').uploadify({
+        width : 120,
+        height : 120,
+        // 隐藏文字
+        buttonText : '',
+        // 隐藏进图条
+        itemTemplate : '<span></span>',
+        swf: '/public/assets/uploadify/uploadify.swf',
+        uploader : '/api/uploader/avatar',
+        fileObjName : 'tc_avatar',
+        onUploadSuccess : function (a, b) {
+          b=b.trim();
+          b = JSON.parse(b);
+          $('.preview img').attr('src',b.result.path);
+        }
+      });
     }
   });
 })
